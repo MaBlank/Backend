bd377396b0343daed3edfa302ccf45d9a86f8568
Matthias Blank <Matthias.Blank2@gmx.net>
2023-07-21 12:03:02 +0200

    Anpassung txt Verarbeitung und Frontend Refactoring

--- src/main/java/com/example/backendfachpraktikumrefactored/Controller.java
+++ src/main/java/com/example/backendfachpraktikumrefactored/Controller.java
@@ -84,7 +84,7 @@ public class Controller {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    // Test
+    // Te
     @PostMapping("/uploadJson")
     public ResponseEntity<?> uploadJson(@RequestBody Document documents) {
         try {
